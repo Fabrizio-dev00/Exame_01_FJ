29ce917336fdc29d938975eed5fd9ae92de9a037
Tecsup <fabrizio.jimenez@tecsup.edu.pe>
2025-09-17 16:25:34 -0500

    views and urls

---
+++ gestor_blog/urls.py
@@ -0,0 +1,4 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [    path('articulos/', views.listar_articulos, name='listar_articulos')]
--- gestor_blog/views.py
+++ gestor_blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Articulos
 
+def listar_articulos(request):
+    articulos = Articulos.objects.all()
+    return render(request, 'gestor_blog/listar_articulos.html', {'articulos': articulos})
 # Create your views here.
