4d9ddd25efebe6c5b78b9ee3c7ec5b217d475cc9
Tecsup <fabrizio.jimenez@tecsup.edu.pe>
2025-09-17 16:43:15 -0500

    urls y views

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from gestor_blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('gestor_blog')),
+    path('', views.listar_articulos, name='home')
 ]
