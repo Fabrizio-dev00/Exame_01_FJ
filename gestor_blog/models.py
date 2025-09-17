bf3c714931fd380ec5139a3343676783814f1513
Tecsup <fabrizio.jimenez@tecsup.edu.pe>
2025-09-17 16:56:37 -0500

    views y urls gestor blog

--- gestor_blog/models.py
+++ gestor_blog/models.py
@@ -1,16 +1,17 @@
 from django.db import models
 
+class Autores(models.Model):
+    nombre = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.nombre
+    
 class Articulos(models.Model):
     titulo = models.CharField(max_length=50)
     contenido = models.CharField(max_length=200)
     fecha_publicacion = models.DateField()
+    autores = models.ForeignKey(Autores, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.titulo
-
-class Autores(models.Model):
-    nombre = models.CharField(max_length=50)
-    email = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.nombre
